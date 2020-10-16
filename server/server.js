1b14bad340cd9675764804200758c2bb874e75f8
Myron <dubtwee@gmail.com>
2020-10-16 13:20:01 -0500

    creating get route for results

--- server/server.js
+++ server/server.js
@@ -1,17 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
+const randoNum = 0;
+const history = [];
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
+// POST for receiving guesses
+
+// GET results of guesses / also full history
+app.get('/results', (req, res) => {
+  res.send(history);
+});
+
+// generate my random number
+
+// POST to restart with new random number
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log('Server is running on port', PORT);
+});
